a173428a3f9eea634a670a303681352b2a7b4c5d
alxjrvs@gmail.com <alxjrvs@gmail.com>
2021-12-01 16:30:53 -0500

    No longer manually copy p10k

--- install.sh
+++ install.sh
@@ -11,8 +11,6 @@ echo "Copying .asdfrc..."
 ln -s ~/dotFiles/.asdfrc ~/.asdfrc
 echo "Copying .gitconfig..."
 ln -s ~/dotFiles/.gitconfig ~/.gitconfig
-echo "Copying .p10k.zsh..."
-ln -s ~/dotFiles/.p10k.zsh ~/.p10k.zsh
 echo "Installing breww.."
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo "Run brew bundle once setup concludes. Welcome to Mac!"
