40f3b5dd49633ff0d8e0ff4e3b3b70f6f336ec36
alxjrvs@gmail.com <alxjrvs@gmail.com>
2021-12-01 20:34:04 -0500

    More lil Fixes, Asdf

--- install.sh
+++ install.sh
@@ -4,16 +4,55 @@
 
 echo "Distributing dotFiles...."
 echo "Copying .zshrc..."
-ln -s ~/dotFiles/.zshrc ~/.zshrc
+ln -sfn ~/dotFiles/.zshrc ~/.zshrc
 echo "Installing OhMyZsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "Setting up Zsh Suggestions"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
+
+echo "Setting up Zsh Plugins..."
+if [ ! -f ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]
+then
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+else
+    echo "Zsh Suggestions already installed, Skipping"
+fi
+
+if [ ! -f f ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z ]
+then
+  git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
+else
+    echo "Zsh-z already installed, skipping"
+fi
+
 echo "Copying .asdfrc..."
-ln -s ~/dotFiles/.asdfrc ~/.asdfrc
+ln -sfn ~/dotFiles/.asdfrc ~/.asdfrc
 echo "Copying .gitconfig..."
-ln -s ~/dotFiles/.gitconfig ~/.gitconfig
-echo "Installing breww.."
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo "Run brew bundle once setup concludes. Welcome to Mac!"
+ln -sfn ~/dotFiles/.gitconfig ~/.gitconfig
+
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+
+if ! exists brew
+then
+  echo "Installing brew.."
+  bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+echo "Running brew bundle..."
+brew bundle
+
+echo "Installing Ruby..."
+asdf plugin add ruby
+asdf install ruby 3.0.3
+asdf global ruby 3.0.3
+
+echo "Installing Nodejs..."
+asdf plugin add nodejs
+asdf install nodejs 16.9.0
+asdf global nodejs 16.9.0
+
+echo "GH auth login"
+gh auth login
